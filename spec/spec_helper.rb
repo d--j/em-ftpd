64e7b901866ad0065eff9f847e9fc364722d7706
Daniel Jagszent <daniel@jagszent.de>
2013-03-21 01:44:10 +0100

    enable "focus: true" filtering for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ require 'em-ftpd'
 # in spec/support/ and its subdirectories.
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
 
-#RSpec.configure do |config|
-#  config.include ReaderSpecHelper
-#end
+RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  # config.include ReaderSpecHelper
+end
